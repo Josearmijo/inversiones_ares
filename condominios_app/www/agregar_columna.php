cb38816be0854af9eeb824f11c9a10214d741bea
Josearmijo <josearmijo99@gmail.com>
2026-07-01 18:35:40 -0400

    Delete condominios_app directory

--- condominios_app/www/agregar_columna.php
+++
@@ -1,4 +0,0 @@
-<?php
-$conn = new PDO("mysql:host=localhost;dbname=inversiones_ares_db", "root", "");
-$conn->exec("ALTER TABLE usuarios ADD COLUMN locatario_id INT NULL AFTER activo");
-echo "Columna agregada";
